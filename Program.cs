358bedd49277f03cd467c42b17968d1016a7d909
tornike nafetvaridze <tornike.nafetvaridze@gmail.com>
2022-05-02 03:13:41 +0400

    using random temperature, that using strategy pattern to provide appropriate comment for temperature

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using BussinessLayer;
+Random rnd = new Random();
+
+int temperature  = rnd.Next(0, 30);
+
+
+if (temperature > 15)
+{
+    CommentContext first = new CommentContext(new FreshStrategy());
+    first.ChooseComment("moscow");
+} else
+{
+    CommentContext second = new CommentContext(new WarmlyStrategy());
+    second.ChooseComment("1");
+}
+
+
+
