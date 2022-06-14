d24d9cf318b1562dad0b5a52ed9c0ff9c73f4408
tornike nafetvaridze <tornike.nafetvaridze@gmail.com>
2022-06-14 15:50:30 +0400

    utonet.60

--- WeatherForecast/ConsoleApp/Program.cs
+++ WeatherForecast/ConsoleApp/Program.cs
@@ -1,122 +1,112 @@
-﻿
+﻿using BL;
+using BL.CommandPattern;
+using BL.Interfaces;
+using DAL.data;
+using DAL.IRepositories;
+using DAL.TypeRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
-using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
+var builder = new ConfigurationBuilder();
+BuildConfig(builder);
 
-namespace ConsoleApp
-{
-    public class Program
+Log.Logger = new LoggerConfiguration()
+   .ReadFrom.Configuration(builder.Build())
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+Log.Logger.Information("Application Starting");
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) =>
     {
-        static async Task Main(string[] args)
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WeatherDb;Trusted_Connection=True;MultipleActiveResultSets=true"));
+        services.AddScoped<IWeatherRepository, WeatherRepository>();
+        services.AddTransient<IWeatherService, WeatherService>();
+    })
+    .UseSerilog()
+    .Build();
+
+var svc = ActivatorUtilities.CreateInstance<WeatherService>(host.Services);
+WeatherService svc1 = ActivatorUtilities.CreateInstance<WeatherService>(host.Services);
+int nav = 0;
+Console.WriteLine("please choose numbers 1-3: 1. current weather by city, 2.weather forecast by city and number of days 3. exit.");
+
+int.TryParse(Console.ReadLine(), out nav);
+
+while (nav != 1 || nav != 2 || nav != 3)
+{
+    Invoker invoker = new Invoker();
+
+    string city;
+    if (nav == 1)
+    {
+        ICommand command = invoker.GetCommand("1");
+        command.Execute();
+        city = Console.ReadLine();
+        city = Regex.Replace(city, @"\s+", "");
+        if (city.Contains(","))
         {
-            //    var builder = new ConfigurationBuilder();
-            //    BuildConfig(builder);
+            string[] namesArray = city.Split(',');
 
-            //    Log.Logger = new LoggerConfiguration()
-            //       .ReadFrom.Configuration(builder.Build())
-            //        .Enrich.FromLogContext()
-            //        .WriteTo.Console()
-            //        .CreateLogger();
+            var dateobjects = await svc1.GetMaxCurrentTemperature(namesArray);
 
-            //    Log.Logger.Information("Application Starting");
+            foreach (var item in dateobjects.Temperatures)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine($"failed {dateobjects.Failed}, Canceled: {dateobjects.Canceled}");
+        }
+        else
+        {
+            while (string.IsNullOrWhiteSpace(city) || city.Length < 2)
+            {
+                Console.WriteLine("please enter valid city name: ");
+                city = Console.ReadLine();
+            }
 
-            //    var host = Host.CreateDefaultBuilder()
-            //        .ConfigureServices((context, services) =>
-            //        {
-            //            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=WeatherDb;Trusted_Connection=True;MultipleActiveResultSets=true"));
-            //            services.AddScoped<IWeatherRepository, WeatherRepository>();
-            //            services.AddTransient<IWeatherService, WeatherService>();
-            //        })
-            //        .UseSerilog()
-            //        .Build();
-
-            //    var svc = ActivatorUtilities.CreateInstance<WeatherService>(host.Services);
-            //    WeatherService svc1 = ActivatorUtilities.CreateInstance<WeatherService>(host.Services);
-            //    int nav = 0;
-            //    Console.WriteLine("please choose numbers 1-3: 1. current weather by city, 2.weather forecast by city and number of days 3. exit.");
-
-            //    int.TryParse(Console.ReadLine(), out nav);
-
-            //    while (nav != 1 || nav != 2 || nav != 3)
-            //    {
-            //        Invoker invoker = new Invoker();
-
-            //        string city;
-            //        if (nav == 1)
-            //        {
-            //            ICommand command = invoker.GetCommand("1");
-            //            command.Execute();
-            //            city = Console.ReadLine();
-            //            city = Regex.Replace(city, @"\s+", "");
-            //            if (city.Contains(","))
-            //            {
-            //                string[] namesArray = city.Split(',');
-
-            //                var dateobjects = await svc1.GetMaxCurrentTemperature(namesArray);
-
-            //                foreach (var item in dateobjects.Temperatures)
-            //                {
-            //                    Console.WriteLine(item.ToString());
-            //                }
-            //                Console.WriteLine($"failed {dateobjects.Failed}, Canceled: {dateobjects.Canceled}");
-            //            }
-            //            else
-            //            {
-            //                while (string.IsNullOrWhiteSpace(city) || city.Length < 2)
-            //                {
-            //                    Console.WriteLine("please enter valid city name: ");
-            //                    city = Console.ReadLine();
-            //                }
-
-            //                var tmpDegreesC = svc1.AddWeather(city).Result;
-            //                Console.WriteLine($"Current temperature is {tmpDegreesC}°C in {city}");
-            //                Console.ReadKey();
-            //            }
-            //        }
-            //        if (nav == 2)
-            //        {
-            //            ICommand commands = invoker.GetCommand("2");
-            //            commands.Execute();
-            //            city = Console.ReadLine();
-
-            //            Console.WriteLine("enter Days of forecast: ");
-            //            //needs to be checked
-            //            int daysofforecast = int.Parse(Console.ReadLine());
-            //            var tmpDegreesC = svc1.GetWeatherForecast(city, daysofforecast).Result;
-            //            foreach (KeyValuePair<double, string> author in tmpDegreesC)
-            //            {
-            //                Console.WriteLine("in {0} is : {1} , Comment: {2}",
-            //                    city, author.Key, author.Value);
-            //            }
-            //            Console.ReadKey();
-            //        }
-            //        if (nav == 3)
-            //        {
-            //            ICommand command = invoker.GetCommand("1");
-            //            command.Execute();
-            //            command = invoker.GetCommand("Stop");
-            //            command.Execute();
-            //            Console.ReadLine();
-            //            break;
-            //        }
-
-            //    }
-            //}
-            //static void BuildConfig(IConfigurationBuilder builder)
-            //{
-            //    builder.SetBasePath(Directory.GetCurrentDirectory())
-            //        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-            //        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
-            //        .AddEnvironmentVariables();
-            //}
+            var tmpDegreesC = svc1.AddWeather(city).Result;
+            Console.WriteLine($"Current temperature is {tmpDegreesC}°C in {city}");
+            Console.ReadKey();
         }
     }
+    if (nav == 2)
+    {
+        ICommand commands = invoker.GetCommand("2");
+        commands.Execute();
+        city = Console.ReadLine();
+
+        Console.WriteLine("enter Days of forecast: ");
+        //needs to be checked
+        int daysofforecast = int.Parse(Console.ReadLine());
+        var tmpDegreesC = svc1.GetWeatherForecast(city, daysofforecast).Result;
+        foreach (KeyValuePair<double, string> author in tmpDegreesC)
+        {
+            Console.WriteLine("in {0} is : {1} , Comment: {2}",
+                city, author.Key, author.Value);
+        }
+        Console.ReadKey();
+    }
+    if (nav == 3)
+    {
+        ICommand command = invoker.GetCommand("1");
+        command.Execute();
+        command = invoker.GetCommand("Stop");
+        command.Execute();
+        Console.ReadLine();
+        break;
+    }
+
+}
+        static void BuildConfig(IConfigurationBuilder builder)
+{
+    builder.SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+        .AddEnvironmentVariables();
 }
