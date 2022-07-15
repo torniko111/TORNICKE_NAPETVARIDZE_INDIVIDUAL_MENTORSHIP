12116641bcd185421302932b19e5f7c46f81548e
tornike nafetvaridze <tornike.nafetvaridze@gmail.com>
2022-07-15 18:23:18 +0400

    Added Working With primitives, working with strings.

---
+++ NetPrimitives/NETdatatypes.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Text;
+
+namespace NetPrimitives
+{
+    [TestClass]
+    public class NETdatatypes
+    {
+        //Task N1 working With primitives, 
+        [TestMethod]
+        public void Precision_Loss_Double_Decimal()
+        {
+            //Arrange
+            double d = 1004233 / (double)1000000;
+            decimal dec = 1004233 / (decimal)1000000;
+
+            //Act
+            double d1 = d - Math.Truncate(d);
+            decimal dec1 = dec - Math.Truncate(dec);  
+            Console.WriteLine("D2 = " + d1);
+            double d2 = d1 * (double)1000000;
+            decimal dec2 = dec1 * (decimal)1000000;
+            Console.WriteLine("D3 = " + d2);
+
+            //Assert
+            Assert.AreNotEqual(0.004233M, d1);
+            Assert.AreNotEqual(4233M, d2);
+            Assert.AreEqual(0.004233M, dec1);
+            Assert.AreEqual(4233M, dec2);
+        }
+
+        //Task N2 working With strings 
+        [TestMethod]
+        public void String_Vs_Stringbuilder_Immutability()
+        {
+            //Arrange
+            string s = "exadel";           
+            StringBuilder sb = new StringBuilder();
+
+            //Act
+            s.Insert(0, "exadel georgia");
+            sb.Insert(0, "exadel georgia");
+            
+            //Assert
+            Assert.AreEqual(s, "exadel");
+            Assert.AreEqual(sb.ToString(), "exadel georgia");
+        }
+
+    }
+}
---
+++ NetPrimitives/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NetPrimitives")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NetPrimitives")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e1b0a989-a402-46eb-9685-eefad81b3048")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
