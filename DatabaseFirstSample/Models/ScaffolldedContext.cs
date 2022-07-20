91ef5d748be65e72ca94fa1d2561e4ef3401569e
tornike nafetvaridze <tornike.nafetvaridze@gmail.com>
2022-07-20 21:16:32 +0400

    Generetad DbContext.
    using semaphoreslim for concurency example,
    also using task<ienumerable> task<string> for the same resulting.

---
+++ DatabaseFirstSample/Models/ScaffolldedContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DatabaseFirstSample.Models
+{
+    public partial class ScaffolldedContext : DbContext
+    {
+        public ScaffolldedContext()
+        {
+        }
+
+        public ScaffolldedContext(DbContextOptions<ScaffolldedContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Blog> Blogs { get; set; } = null!;
+        public virtual DbSet<Post> Posts { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Scaffollded;Trusted_Connection=False;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>(entity =>
+            {
+                entity.Property(e => e.Name).HasMaxLength(200);
+
+                entity.Property(e => e.Url).HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.Property(e => e.Content).HasColumnType("ntext");
+
+                entity.Property(e => e.Title).HasMaxLength(200);
+
+                entity.HasOne(d => d.Blog)
+                    .WithMany(p => p.Posts)
+                    .HasForeignKey(d => d.BlogId)
+                    .HasConstraintName("FK_dbo.Posts_dbo.Blogs_BlogId");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ DatabaseFirstSample/Program.cs
@@ -0,0 +1,50 @@
+﻿using DatabaseFirstSample.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+var db = new ScaffolldedContext();
+SemaphoreSlim _semaphoregate = new SemaphoreSlim(1);
+
+// Create and save a new Blog
+Console.Write("Enter a name for a new Blog: ");
+var name = Console.ReadLine();
+
+var blog = new Blog { Name = name };
+db.Blogs.Add(blog);
+db.SaveChanges();
+
+// Display all Blogs from the database
+
+async Task<IEnumerable<string>> BlogsName()
+{
+    await _semaphoregate.WaitAsync();
+    var result = await db.Blogs.Select(b => b.Name).ToListAsync();
+    _semaphoregate.Release();
+
+    return result;
+}
+
+async Task<IEnumerable<Blog>> Blogs()
+{
+    await _semaphoregate.WaitAsync();
+    var result = await db.Blogs.Select(b => b).ToListAsync();
+    _semaphoregate.Release();
+    return result;
+}
+
+var blogsString = await BlogsName();
+var blogs = await Blogs();
+
+Console.WriteLine("All blogs in the database:");
+foreach (var item in blogsString)
+{
+    Console.WriteLine(item);
+}
+
+foreach (var item in blogs)
+{
+    Console.WriteLine(item.Name);
+}
+
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
